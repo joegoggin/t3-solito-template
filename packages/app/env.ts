1356c0358ede9260f0073e3fcb283dd6d0550a0a
joegoggin <joegoggin27@gmail.com>
2023-08-18 15:21:37 -0400

    Delete env.ts

--- packages/app/env.ts
+++
@@ -1,8 +0,0 @@
-import { Platform } from "react-native";
-
-const tunnel = "https://eleven-rules-play.loca.lt";
-
-export const API =
-    Platform.OS === "web"
-        ? "http://localhost:3000/api/trpc"
-        : `${tunnel}/api/trpc`;
